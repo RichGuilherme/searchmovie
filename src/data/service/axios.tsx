4917db327b9dca9679fb5d5d1d8db11940265e34
RichGuilherme <richard.guilhermeAS@hotmail.com>
2023-07-07 15:29:45 -0300

    feat: Sistema de cache contra duplicada

--- src/data/service/axios.tsx
+++ src/data/service/axios.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const Key = process.env.NEXT_PUBLIC_API_URL
 
@@ -12,10 +12,20 @@ const options = {
   }
 };
 
+interface Cache {
+  [url: string]: AxiosResponse;
+}
 
+const cache:Cache = {};
 export const DatasApi = async (url: string) => {
     try {
+      if (cache[url]) {
+        return cache[url]
+       }
+
        const response = await axios.get(url, options)
+       cache[url] = response.data
+
        return response.data
     } catch(erro) {
       
