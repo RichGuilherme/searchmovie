ad5dc70ec69a02b754b27ac552e9afb6aecb54b0
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-02-21 23:19:11 -0300

    fix: erro do typescript

--- src/components/details/TopCast.tsx
+++ src/components/details/TopCast.tsx
@@ -1,37 +1,39 @@
 import { topCastProps } from "@/@types/apiInformation"
 
 export const TopCast = ({ cast, loadingCrew }: topCastProps) => {
- 
+
 
   return (
     <section className="w-[76%] mx-auto mt-14">
       <h2 className="text-3xl font-medium font-Nunito">Top Atores</h2>
 
-        <div className="flex flex-row gap-14 w-full mt-6 overflow-hidden">
-          {!loadingCrew ? (
-            <>
-              {cast && cast?.actors && cast.map((actors, i) => (
+      <div className="flex flex-row gap-14 w-full mt-6 overflow-hidden">
+        {!loadingCrew ? (
+          <>
+            {cast && cast.map((actors, i) => (
+              <div
+                key={i}
+                className="flex flex-col items-center gap-y-3">
+
                 <div
-                  key={i}
-                  className="flex flex-col items-center gap-y-3">
+                  style={{
+                    backgroundImage: actors?.profile_path
+                      ? `url(https://image.tmdb.org/t/p/w300${actors.profile_path})`
+                      : 'none'
+                  }}
+                  className="w-[175px] h-[175px] rounded-full bg-no-repeat bg-center bg-[cover]"></div>
 
-                  <div
-                    style={{
-                      backgroundImage: `url(https://image.tmdb.org/t/p/w300${actors?.profile_path})`
-                    }}
-                    className="w-[175px] h-[175px] rounded-full bg-no-repeat bg-center bg-[cover]"></div>
-
-                  <div className="flex flex-col items-center gap-1">
-                    <p className="text-[19px] text-center text-textColors-200">{actors.character}</p>
-                    <span className="text-[19px] text-center text-textColors-100">{actors.name}</span>
-                  </div>
+                <div className="flex flex-col items-center gap-1">
+                  <p className="text-[19px] text-center text-textColors-200">{actors.character}</p>
+                  <span className="text-[19px] text-center text-textColors-100">{actors.name}</span>
                 </div>
-              ))}
-            </>
+              </div>
+            ))}
+          </>
 
-          ) : ""
-          }
-        </div>
+        ) : ""
+        }
+      </div>
 
     </section>
   )
